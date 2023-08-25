fc6bd9ca75f6919492b7151e3441494683cb84f5
Tejas <kumarley.tejas7.tk15@gmail.com>
2023-08-25 17:20:02 +0530

    initial commit

---
+++ src/config.js
@@ -0,0 +1,15 @@
+const BASE_URL = "https://api.themoviedb.org/3/";
+const API_KEY_PARAM = "?api_key=11b7d3f9368869b6fb62ab44ab899e55";
+const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original";
+const SMALL_IMAGE_COVER_BASE_URL = "https://image.tmdb.org/t/p/w300";
+const GENRE = "https://api.themoviedb.org/3/genre/list";
+const MOVIE = "https://api.themoviedb.org/3/discover/movie";
+
+export {
+  BASE_URL,
+  API_KEY_PARAM,
+  BACKDROP_BASE_URL,
+  SMALL_IMAGE_COVER_BASE_URL,
+  GENRE,
+  MOVIE,
+};
